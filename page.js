86ffa943011b99a7267dc400b5abecc80fd4da84
Nawrin Tarannum <nawrintarannum30@gmail.com>
2026-09-25 13:23:21 +0600

    page

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
-import HomePage from "../components/HomePage";
+import MyPlanPage from "../../components/MyPlanPage";
 
 export default function Page() {
-  return <HomePage />;
+  return <MyPlanPage />;
 }
